571581f2ef4a4443568aee086103b9d7420a5ad4
noelle203 <964454708@qq.com>
2026-08-23 01:59:12 +0800

    fix: 更新试炼离场提示

--- os/char/chara_move.js
+++ os/char/chara_move.js
@@ -78,7 +78,7 @@ CHARACTER.prototype.moveto = function (rm, leave_msg, in_msg, dir) {
 
     // 真意试炼只能通过动作栏结算或退出；拦截 goto/直传等绕过房间出口的移动。
     if (this.is_player && cur_room && cur_room.zhenyi_trial_room && this.query_temp("zy_trial_active", "")) {
-        this.notify("试炼尚未结束，只能使用动作栏的完成副本或退出副本。");
+        this.notify("试炼尚未结束，只能使用动作栏的退出副本。");
         return false;
     }
 
